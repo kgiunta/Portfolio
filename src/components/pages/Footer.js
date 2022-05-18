26a0cd01a9b5db721b6dc1212d08a556dd676f8f
Kgiunta <giunta.kyle@yahoo.com>
2022-05-18 15:27:25 -0400

    email link

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import linkedin from "../../images/Linkedin-Icon.png";
 import github from "../../images/github.png";
+import emails from "../../images/email-logo-png.png";
 
 function Footer() {
   return (
@@ -15,6 +16,13 @@ function Footer() {
         </a>
         <a href="https://github.com/kgiunta" target="_blank" rel="noreferrer">
           <img className="github" src={github} alt="github logo" />{" "}
+        </a>
+        <a
+          href="mailto:abc@example.com?subject = Saw Your Portfolio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="emails" src={emails} alt="email Kyle link" />
         </a>
       </footer>
     </div>
